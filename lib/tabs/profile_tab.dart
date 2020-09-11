3713f89af030f68a8f43c9e49353365345e7d7c1
Hakkinen Diniz <santoshakkinen7@gmail.com>
2020-09-11 17:02:09 -0300

    Tela Login

--- lib/tabs/profile_tab.dart
+++ lib/tabs/profile_tab.dart
@@ -27,11 +27,70 @@ Widget build(BuildContext context) {
               ),
               centerTitle: true,
             ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Criar Conta",style: TextStyle(fontSize: 15.0),),
+                textColor: Colors.white,
+                onPressed: (){},
+              ),
+            ],
           ),
         ],
       ),
+      Form(
+        child: ListView(
+          padding: EdgeInsets.fromLTRB(32, 126, 32, 16),
+          children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(
+                  hintText: "E-mail",
+                  border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(36)
+                ),
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 16, bottom: 10),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  hintText: "Senha",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(36)
+                  )
+              ),
+              obscureText: true,
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: FlatButton(
+                onPressed: (){},
+                child: Text("Esqueci minha senha", textAlign: TextAlign.right,
+                ),
+                padding: EdgeInsets.zero,
+              ),
+            ),
+            SizedBox(height: 16.0,
+            ),
+            SizedBox(height: 55.0,
+            child: RaisedButton(
+              child: Text("Entrar",
+                style: TextStyle(
+                  fontSize: 18.0,
+                ),
+              ),
+              textColor: Colors.white,
+              color: Colors.black26,
+              onPressed: (){},
+            ),
+            ),
+          ],
+        ),
+      ),
     ],
   );
+
 }
 
 }
