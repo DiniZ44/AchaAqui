cc5399451b7b45d08e55e49d36f50cb9d8b61198
Hakkinen Diniz <santoshakkinen7@gmail.com>
2020-09-23 18:08:13 -0300

    Atualizações

--- lib/tabs/business_tab.dart
+++ lib/tabs/business_tab.dart
@@ -5,8 +5,48 @@ class BusinessTab extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black26,
+      body: Stacks(),
     );
   }
+}
+Widget Stacks(){
+  return Stack(
+    children: <Widget>[
+      ListView(
+        padding: EdgeInsets.fromLTRB(42, 66, 42, 16),
+        children: <Widget>[
+          SizedBox(height: 35.0,
+            child: RaisedButton(
+              child:
+              Text("Cadastrar Négocio",
+                style: TextStyle(
+                  fontSize: 18.0,
+                ),
+              ),
+              textColor: Colors.white,
+              color: Colors.black12,
+              onPressed: (){},
+            ),
+          ),
+          SizedBox(height: 25.0,
+          ),
+          SizedBox(height: 35.0,
+            child: RaisedButton(
+              child:
+              Text("Plano Premium",
+                style: TextStyle(
+                  fontSize: 18.0,
+                ),
+              ),
+              textColor: Colors.white,
+              color: Colors.black12,
+              onPressed: (){},
+            ),
+          ),
+        ],
+      ),
+    ],
+  );
 }
 
 
