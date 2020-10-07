2555807f6ed8d342145ba95ea637a1e58a9500fb
Hakkinen Diniz <santoshakkinen7@gmail.com>
2020-10-07 13:20:50 -0300

    Mudando de Django para Firebase

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,12 +1,25 @@
+import 'package:acha_aqui/tiles/category_tile.dart';
 import 'package:flutter/material.dart';
-
-import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomeTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black26,
+    return FutureBuilder<QuerySnapshot>(
+      future: Firestore.instance.collection("Categoria").getDocuments(),
+      builder: (context, snapshot){
+        if(!snapshot.hasData)
+          return Center(child: CircularProgressIndicator(),);
+      else{
+        return ListView(
+          children: snapshot.data.documents.map(
+              (doc){
+                return CategoryTile(doc);
+              }
+          ).toList(),
+        );
+        }
+        },
     );
   }
 }
