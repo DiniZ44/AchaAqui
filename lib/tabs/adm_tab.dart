50cd1aced5929502867d0516ed17855ee167d6c5
Hakkinen Diniz <santoshakkinen7@gmail.com>
2020-10-07 17:02:47 -0300

    Contatar adm

--- lib/tabs/adm_tab.dart
+++ lib/tabs/adm_tab.dart
@@ -5,8 +5,47 @@ class AdmTab extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black26,
+        body: VariasStacks(),
     );
   }
 }
 
+Widget VariasStacks(){
+  return Stack(
+    children: <Widget>[
+      Form(
+        child: ListView(
+          padding: EdgeInsets.fromLTRB(32, 66, 32, 86),
+          children: <Widget>[
+          TextFormField(
+          decoration: InputDecoration(
+          hintText: "Reporte aqui, sujestões, problemas, duvidas e bugs. ",
+          hintMaxLines: 30,
+          helperMaxLines: 30,
+          border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(36)
+          ),
+          ),
+          ),
+          Padding(
+          padding: EdgeInsets.only(top: 16, bottom: 10),
+        ),
+            SizedBox(height: 55.0,
+              child: RaisedButton(
+                child: Text("Enviar",
+                  style: TextStyle(
+                    fontSize: 18.0,
+                  ),
+                ),
+                textColor: Colors.white,
+                color: Colors.black26,
+                onPressed: (){},
+              ),
+            ),
+        ],
+        ),
+      )
+    ],
+  );
+}
 
